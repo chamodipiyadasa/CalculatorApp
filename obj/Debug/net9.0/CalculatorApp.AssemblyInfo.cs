2333ab29afeb78fd8c0168de9ff4b1a850c4a1e9
chamodi <chamodipiyadasa@gmail.com>
2025-07-10 01:06:34 +0530

    functional app

--- obj/Debug/net9.0/CalculatorApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/CalculatorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e6946edbf0270abe55ee5dab4e5ddeda00f592d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b585a25e10de3f6224c802b192449f7087f5d6ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
